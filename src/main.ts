aa19d65ebe8217b85487083d1d6b750ce3d17b60
檬 柠 <eefen88@163.com>
2025-07-01 23:28:00 +0800

    refactor(src): 优化页面标题设置并调整 Downloads 组件
    - 移除 main.ts 中的路由守卫，统一在 router/index.ts 中处理
    - 更新路由守卫逻辑，优化页面标题设置
    - 在 Downloads 组件中添加展开行功能，并限制同时只能展开一行
    - 使用 NCollapseTransition 替代

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import packageData from '../package.json'
 import { 
   create,
   NMessageProvider,
   NDialogProvider,
   NLoadingBarProvider
 } from 'naive-ui'
-
-router.beforeEach((to, _from, next) => {
-  // 设置文档标题
-  document.title = to.meta.title ?
-      `${to.meta.title} - ${packageData.title2}` : // 自定义标题格式
-      `${packageData.title2}` // 默认标题
-  next()
-})
 
 const naive = create({
   components: [NMessageProvider, NDialogProvider, NLoadingBarProvider]
